d0a1068991e75c1c744c668c7db0801b7f40f2bb
Erkutbek <erkubekt@gmail.com>
2023-08-23 21:59:14 +0600

    change header and add navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 
 import './App.css';
-import Header from './components/Header/Header';
-import NavAssistant from './components/NavAssistant/NavAssistant';
-<<<<<<< HEAD
-import Basket from './pages/Basket/Basket';
-=======
+import { Routes,Route } from 'react-router-dom'
+import Layout from './components/Layout/Layout';
+import Loader from './components/Ui/Loader/Loader';
 
->>>>>>> e129c57e100cd1995d82d9b95170ea8ba56735b7
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <NavAssistant title="ЦЕНЫ НА ШКОЛЬНУЮ СКАМЬЮ"/>
-<<<<<<< HEAD
-      <Basket/>
-=======
->>>>>>> e129c57e100cd1995d82d9b95170ea8ba56735b7
+      <Routes>
+        <Route loader={<Loader/>}  element={<Layout/>} >
+            <Route index  path='/' element={<h1>Hello</h1>}/>
+            <Route path='/catalog' element={<h1>Lorem </h1>}/>
+        </Route>
+        <Route path='*' element={<h1>Not found page 404</h1>}/>
+      </Routes> 
     </div>
   );
 }
